809f61037a69f225e95bdd670387feb1e31454e4
Billionaireswag1 <dineo.lesab@gmail.com>
2026-03-27 20:22:37 +0200

    Built navbar layout with grid, searh bar, and right-side buttons

--- vite-project/src/Components/Navbar.tsx
+++ vite-project/src/Components/Navbar.tsx
@@ -1,22 +1,41 @@
-
 import "./Navbar.css";
 import { FaSearch } from "react-icons/fa";
+import logo from "../assets/fcc_primary_large.svg";
+
 const Navbar = () => {
   return (
+    <nav className="navbar">
+      <div className="navbar-inner">
+        <div className="navbar-left">
+          <div className="search-container">
+            <FaSearch className="search-icon" />
+            <input
+              type="search"
+              placeholder="Search 12,400+ news articles, tutorials and books"
+            />
+          </div>
+        </div>
 
-<nav className="navbar">
-  <div className="navbar-inner">
-    {/* LEFT (Search bar) */}
+        <div className="navbar-center">
+          <img src={logo} alt="freeCodeCamp logo" />
+        </div>
 
-    <div className="search-container">
-     <FaSearch className="search-icon" />
+        <div className="navbar-right">
+          <button className="icon-button" aria-label="language">
+            🌐
+          </button>
 
-<input type='search' placeholder="Search 12,400+news articles, tutorials and books"/>
+          <button className="menu-button">
+            Menu
+          </button>
+
+          <button className="icon-button" aria-label="profile">
+            👤
+          </button>
+        </div>
       </div>
-      </div>
-      
-</nav>
-);
+    </nav>
+  );
 };
 
 export default Navbar;
